29ddba2e98ca9d966ee1e5650c00ac1357c23b91
Taher Mun <tahermun@gmail.com>
2020-09-23 12:51:43 -0400

    updated vcf_to_rowbowt to include marker window size

--- scripts/vcf_to_rowbowt.sh
+++ scripts/vcf_to_rowbowt.sh
@@ -4,6 +4,7 @@ FASTA=$1
 VCF=$2
 SAMPLES=$3
 OUT=${4:-out}
+WSIZE=${5:-10}
 
-python pfbwt-f/vcf_to_bwt.py --clean --keep_parse -r -m -S $SAMPLES -o ${OUT} ${FASTA} ${VCF}
+vcf_to_bwt.py --clean --keep_parse -r -m -S $SAMPLES -o ${OUT} --ma_wsize ${WSIZE} ${FASTA} ${VCF}
 rb_build -s -m -o $OUT $OUT
